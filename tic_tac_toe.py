0a91dcd66b28c6ed21b63f445486fdd2bbb65fda
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2019-12-12 23:45:11 -0500

    Refactor code into a class

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,74 +1,81 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
-window = Tk()
 
-window.title("Welcome to The Gaming world TIC-Tac-Toe ")
-window.geometry("400x300")
-
-lbl = Label(window, text="Tic-tac-toe Game", font=('Helvetica', '15'))
-lbl.grid(row=0, column=0)
-lbl = Label(window, text="Player 1: X", font=('Helvetica', '10'))
-lbl.grid(row=1, column=0)
-lbl = Label(window, text="Player 2: O", font=('Helvetica', '10'))
-lbl.grid(row=2, column=0)
-
-turn = True
+def main():
+    program = Tic_Tac_Toe()
+    program.window.mainloop()
 
 
-# For first person turn.
-def clicked(id: int):
-    global turn
-    button = buttons[id]
-    if not buttons[id]["text"]:  # For getting the text of a button
-        if turn:
-            button["text"] = "X"
-        else:
-            button["text"] = "O"
-        turn = not turn
-        check()
+class Tic_Tac_Toe:
+    def __init__(self):
+        self.next_move = True
+        self.turn = 1
+        self.window = self.create_window()
+        self.buttons = self.create_buttons()
+
+    def create_window(self):
+        window = tk.Tk()
+        window.title("Welcome to The Gaming world TIC-Tac-Toe ")
+        window.geometry("400x300")
+        lbl = tk.Label(window, text="Tic-tac-toe Game", font=('Futura', '15'))
+        lbl.grid(row=0, column=0)
+        lbl = tk.Label(window, text="Player 1: X", font=('Futura', '10'))
+        lbl.grid(row=1, column=0)
+        lbl = tk.Label(window, text="Player 2: O", font=('Futura', '10'))
+        lbl.grid(row=2, column=0)
+        return window
+
+    def create_buttons(self):
+        buttons = dict()
+        counter = 1
+        for row in range(1, 4):
+            for column in range(1, 4):
+                buttons[counter] = tk.Button(
+                    self.window,
+                    bg="yellow",
+                    fg="black",
+                    width=3,
+                    height=1,
+                    font=('Futura', '20'),
+                    command=lambda id=counter: self.clicked(id))
+                buttons[counter].grid(column=column, row=row)
+                counter += 1
+        return buttons
+
+    def clicked(self, id: int):
+        button = self.buttons[id]
+        if not self.buttons[id]["text"]:  # For getting the text of a button
+            if self.next_move:
+                button["text"] = "X"
+            else:
+                button["text"] = "O"
+            self.next_move = not self.next_move
+            self.check()
+
+    def check(self):
+        winning_combinations = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
+                                (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+        marks = ('X', 'O')
+
+        for combination in winning_combinations:
+            for mark in marks:
+                if all(
+                    (self.buttons[id]['text'] == mark for id in combination)):
+                    self.win(mark)
+                    exit(0)
+        if self.turn == 9:
+            messagebox.showinfo('Tie', 'Try again!')
+            self.window.destroy()
+            exit(0)
+
+        self.turn += 1
+
+    def win(self, player):
+        ans = "Game complete " + player + " wins "
+        messagebox.showinfo("Congratulations", ans)
+        self.window.destroy()  # is used to close the program
 
 
-flag = 1
-
-
-def check():
-    global flag
-    flag += 1
-
-    winning_combinations = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
-                            (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
-    marks = ('X', 'O')
-
-    for combination in winning_combinations:
-        for mark in marks:
-            if all((buttons[id]['text'] == mark for id in combination)):
-                win(mark)
-                exit(0)
-    if flag == 10:
-        messagebox.showinfo('Tie', 'Try again!')
-        window.destroy()
-        exit(0)
-
-
-def win(player):
-    ans = "Game complete " + player + " wins "
-    messagebox.showinfo("Congratulations", ans)
-    window.destroy()  # is used to close the program
-
-
-buttons = dict()
-counter = 1
-for row in range(1, 4):
-    for column in range(1, 4):
-        buttons[counter] = Button(window,
-                                  bg="yellow",
-                                  fg="Black",
-                                  width=3,
-                                  height=1,
-                                  font=('Helvetica', '20'),
-                                  command=lambda id=counter: clicked(id))
-        buttons[counter].grid(column=column, row=row)
-        counter += 1
-
-window.mainloop()
+if __name__ == '__main__':
+    main()
